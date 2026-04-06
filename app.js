ef279557d6226cf3f10e57da8f24d19c5fd67f53
aravindroy1 <127665017+aravindroy1@users.noreply.github.com>
2026-04-06 22:59:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serves the HTML from the public folder
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello DevOps 🚀</h1><p>Sent from a Node.js Server!</p>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`App is humming along at http://localhost:${PORT}`);
+    console.log(`App running at http://localhost:${PORT}`);
 });
